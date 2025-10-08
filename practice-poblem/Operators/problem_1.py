87d45f31905955bd9e657eb6c070e5da5c46c173
yogesh Budha <yogeshbudha829@gmail.com>
2025-10-08 17:57:24 +0545

    solving some problem on operators

---
+++ practice-poblem/Operators/problem_1.py
@@ -0,0 +1,23 @@
+# Write a program that take a user input of three angles and will find out whether it can form a triangle or not.
+
+# a = int(input("Enter the 1st angle: "))
+# b = int(input("Enter the 2nd angle: "))
+# c = int(input("Enter the 3rd angle: "))
+
+# sum = a+b+c
+
+# if(sum == 180):
+#     print("It form a triangle")
+# else:
+#     print("It doesnot form a triangle")
+
+
+# Write a program that will take user input of cost price and selling price and determines whether its a loss or a profit.
+
+cp = int(input("Enter the cost price: "))
+sp = int(input("Enter the selling price: "))
+
+if(sp<cp):
+    print("loss")
+else:
+    print("profit")
--- practice-poblem/problem_4.py
+++
@@ -1,7 +0,0 @@
-# Use reduce to convert a 2D list to 1D
-import functiontools
-
-list1 = [1,2,3,4,5,6]
-
-temp = functiontools.reduce(lamdba x,y: x+y,list1)
-print(temp)
